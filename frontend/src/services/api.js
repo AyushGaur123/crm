9fd9960dfec7059674904adadb8d51b1a22897e7
AyushGaur123 <ayushgaur914@gmail.com>
2026-08-23 13:41:17 +0530

    update

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "http://localhost:5000/api",
-  baseURL: "https://crm-55c2.onrender.com",
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
